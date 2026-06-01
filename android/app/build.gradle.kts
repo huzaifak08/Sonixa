73735780b219c5aa4949e635f814b2cb1e080b54
huzaifak08 <hk7928042@gmail.com>
2026-06-01 22:58:21 +0500

    new sonixa

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,10 +41,19 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            // Check if key.properties exists, otherwise use Dart Defines/Environment variables
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            } else {
+                // This allows GitHub Actions to inject the values
+                keyAlias = System.getenv("KEY_ALIAS") ?: ""
+                keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+                storeFile = file("upload-keystore.jks")
+                storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
+            }
         }
     }
     buildTypes {
